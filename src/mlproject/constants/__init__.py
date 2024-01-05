ebd58ac9952a411bf4e19b0ed5cba4a6bf3a20f0
Soura10 <souradeeproy.10@gmail.com>
2024-01-05 23:16:23 +0530

    data ingestion test

--- src/mlproject/constants/__init__.py
+++ src/mlproject/constants/__init__.py
@@ -3,3 +3,9 @@ from pathlib import Path
 CONFIG_FILE_PATH = Path("config/config.yaml")
 PARAMS_FILE_PATH = Path("params.yaml")
 SCHEMA_FILE_PATH = Path("schema.yaml")
+
+#MONGO_DATABASE_NAME= "winedata1"
+#MONGO_COLLECTION_NAME="wine1"
+
+#MONGODB_URL="mongodb+srv://soura10:soura10@cluster0.vfi0yui.mongodb.net/?retryWrites=true&w=majority"
+#artifact_folder="artifact"
